0feb7274a8317c484bd505455e6387f727698a6f
Danner Abel Aly Delgado <dannerabelaly@gmail.com>
2022-09-29 10:27:40 -0400

    2do commit actualizado

---
+++ principal.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "********  HOLA MUNDO  ********" << endl;
+	cout << "\nNombre y Apellido: Danner Abel Aly Delgado" << endl;
+	cout << "Telefono: 67914341" << endl;
+	cout << "Fecha de nacimiento: 25 de noviembre de 2002" << endl;
+	cout << "C.I: 12526225" << endl;
+	cout << "Desafio: En este semestre lograr cumplir con el desafío de la " << endl;
+	cout << "elaboración de un videojuego completo aunque me cueste y duela" << endl;
+
+	return 0;
+}
