d6ce46b4e42f6ae63cbf905aed3fb79928ef9876
Danner Abel Aly Delgado <dannerabelaly@gmail.com>
2022-10-01 21:37:16 -0400

    Datos Danner

--- principal.cpp
+++ principal.cpp
@@ -9,8 +9,8 @@ int main() {
 	cout << "Telefono: 67914341" << endl;
 	cout << "Fecha de nacimiento: 25 de noviembre de 2002" << endl;
 	cout << "C.I: 12526225" << endl;
+	cout << "Solo un muchacho de 19 años amante a la producción audiovisual y la programación" << endl;
 	cout << "Desafio: En este semestre lograr cumplir con el desafío de la " << endl;
 	cout << "elaboración de un videojuego completo aunque me cueste y duela" << endl;
-
 	return 0;
-}
+}                                                                                              
